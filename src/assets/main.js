769a7b1f1a7ea47e7b64a7bb8fbba426fbc5e4f8
kasynych <maximbtzt593@gmail.com>
2017-01-24 18:44:34 +0200

    Ajax request

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,13 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/kamitor.json',
+    dataType:'jsonp',
+    success:function(response){
+      response.courses.completed.forEach(function(course){
+        $('#badges').append('<div class="course"><h3>'+course.title+'</h3><img src="'+course.badge+'" /><a href="'+course.url+'" target="_blank" class="btn btn-primary">See Course</a></div>');
+      })
+    }
+  });
 
 });
